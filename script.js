a57fff35a07f0912a1f03d93cd091747b601f1ad
ChouhatsunoAozora <6611110142@mut.ac.th>
2025-07-09 12:38:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function tweetMessage() {
-    const message = "Mei ChouhatsunoAozora\n#MeiBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay3";
+    const message = "Mei ChouhatsunoAozora\n#MeiCGM48\n#คำท้าทายที่ปลายฟ้าConceptDay3";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }
